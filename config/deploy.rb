322ff0b5472f8273db04de4b1695d45f9565cc0b
Hyo Seong Choi <hschoidr@gmail.com>
2015-05-25 12:46:34 +0900

    deploy.rb 파일 정

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,13 +6,10 @@ set :repo_url, "git@github.com:hschoidr/#{fetch(:application)}.git"
 set :deploy_to, "/home/deployer/apps/#{fetch(:application)}"
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
-# set :rbenv_custom_path, '/home/deployer/.rbenv/'
 set :rbenv_ruby, '2.2.2'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default values
-
-# set :rails_env, "production"
 
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
@@ -20,16 +17,11 @@ set :config_files, fetch(:linked_files)
 set :pty, true
 set :keep_releases, 5
 
-# set :default_env , { path: '$PATH:/sbin' }
-
 before 'deploy:check:linked_files', 'config:push'
-
-Rake::Task['deploy:assets:backup_manifest'].clear_actions
 
 namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      execute :sudo, :unicorn, 'restart'
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
